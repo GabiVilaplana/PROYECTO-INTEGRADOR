d1137d4c0174eb9c854b88c087b1ef4595e6e9d3
Sitoosito8 <a20000lex@gmail.com>
2025-10-17 16:29:21 +0200

    Subir cambio relacionado con el index.php primeras pruebas de CONTACTA CON NOSOTROS

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,4 +1,36 @@
 <?php
+$nom = "";
+$email = "";
+$telefon = "";
+$consentimiento = false;
+$cicle = "";
+$errores = [];
 
-echo("Hello World");
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $nom = $_POST["nom"]?? "";
+    $email = $_POST["email"]?? "";
+    $telefon = $_POST["telefon"]?? "";
+    $consentimiento = $_POST["consentimiento"]?? "";
+    $cicle = $_POST["cicle"] ?? "";
+
+
+
+    if (empty($nombre)) {
+        $errores["errorNombre"] = "No introducido ningún nombre";
+    }
+
+    if (empty($email)) {
+        $errores["errorEmail"] = "No introducido ningún Email o este es incorrecto";
+    }
+
+    if (empty($telefon)) {
+        $errores["errorTelefon"] = "Telefono que ha introducido es incorrecto";
+    }
+
+    if (empty($cicle)) {
+        $errores["erroreCicle"] = "Ciclo Introducido es nulo";
+    }
+
+}
 ?>
