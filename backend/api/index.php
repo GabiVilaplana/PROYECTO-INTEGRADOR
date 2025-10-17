1a6b42d65d56cc3483dc5ece7e7313660fefc470
Sitoosito8 <a20000lex@gmail.com>
2025-10-17 17:02:04 +0200

    SUbir cambios sobre indice.php y index.css

--- backend/api/index.php
+++ backend/api/index.php
@@ -8,10 +8,10 @@ $errores = [];
 
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    $nom = $_POST["nom"]?? "";
-    $email = $_POST["email"]?? "";
-    $telefon = $_POST["telefon"]?? "";
-    $consentimiento = $_POST["consentimiento"]?? "";
+    $nom = $_POST["nom"] ?? "";
+    $email = $_POST["email"] ?? "";
+    $telefon = $_POST["telefon"] ?? "";
+    $consentimiento = $_POST["consentimiento"] ?? "";
     $cicle = $_POST["cicle"] ?? "";
 
 
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         $errores["errorEmail"] = "No introducido ningún Email o este es incorrecto";
     }
 
-    if (empty($telefon)) {
+    if (empty($telefon) && strlen(trim($telefon)) !== 9) {
         $errores["errorTelefon"] = "Telefono que ha introducido es incorrecto";
     }
 
@@ -34,3 +34,63 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf8">
+    <title>Proyecto Intermodular</title>
+    <meta name="description" content="Ejercicio de Javascript">
+    <meta name="author" content="Gabi y Alex">
+    <meta name="keywords" content="Venta, Articulos, e-commerce">
+    <link href="../../frontend/HTML/CSS/index.css" rel="stylesheet" type="text/css">
+</head>
+</head>
+
+<body>
+    <h1>Pagina Contactos</h1>
+
+    <label><strong>Nom:</strong></label>
+    <input type="text" id="nom" name="nom" required><br><br>
+    
+    <?php if ((strlen(htmlspecialchars($errores["errorNombre"])) > 0)): ?>
+        <p><?php htmlspecialchars($errores["errorNombre"]) ?></p><br>
+    <?php endif; ?>
+
+    <label><strong>Email: </label>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <?php if ((strlen(htmlspecialchars($errores["errorEmail"])) > 0)): ?>
+        <p><?php htmlspecialchars($errores["errorEmail"]) ?></p><br>
+    <?php endif; ?>
+
+    <label for="cicle"><strong>Cicle Formatiu</strong> </label>
+    <select id="cicle" name="cicle">
+        <option vlaue="ASIX"></option>
+        <option vlaue="DAM"></option>
+        <option vlaue="DAW"></option>
+    </select><br>
+    <p>
+
+        <?php if ((strlen(htmlspecialchars($errores["errorCicle"])) > 0)): ?>
+        <p><?php htmlspecialchars($errores["errorCicle"]) ?></p><br>
+    <?php endif; ?>
+
+    <label><strong>Telefon:</strong></label>
+    <input type="number" id="telefon" name="telefon" required><br><br>
+
+    <?php if ((strlen(htmlspecialchars($errores["errorTelefon"])) > 0)): ?>
+        <p><?php htmlspecialchars($errores["errorTelefon"]) ?></p><br>
+    <?php endif; ?>
+
+    <label><strong>Al enviar este formulario, aceptas el tratamiento de tus datos personales conforme a nuestra Política
+            de Privacidad.</strong></label>
+    <input type="checkbox" id="consentimiento" name="consentimiento" required><br>
+
+    <input href="../../frontend/HTML/index.html" type="submit" id="button" name="button" value="Volver">
+</body>
+</form>
+</body>
+
+</html>
