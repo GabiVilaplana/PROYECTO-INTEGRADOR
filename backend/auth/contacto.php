0a816ad8545ba3dd6acfcb2a8a7022f15b3d2c8f
Sitoosito8 <a20000lex@gmail.com>
2025-11-21 20:34:10 +0100

    Subir cambios sobre los contactos

--- backend/auth/contacto.php
+++ backend/auth/contacto.php
@@ -4,57 +4,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../frontend/HTML/CSS/contacto.css" type="text/css" />
     <title>Pagina Contacto TaskLink</title>
 </head>
 
 <body>
-    <aside class="sidebar">
-        <section class="opcionesContacto">
-            <div class="contacto">
-                <img src="../../frontend/IMG/Instagram.png">
-                <span>Instagram</span>
-            </div>
-            <div class="contacto">
-                <img src="../../frontend/IMG/facebook.png">
-                <span>Facebook</span>
-            </div>
-            <div class="contacto">
-                <img src="../../frontend/IMG/WhatsApp.png">
-                <span>WhatsApp</span>
-            </div>
-            <div class="contacto">
+    <div class="contenedor">
+        <aside class="sidebar">
+            <section class="opcionesContacto">
+                <div class="OtrosContactos">
+                    <h3>Otras Formas de Contacto</h3>
+                </div>
+                <div class="contactoInsta" id="contacto">
+                    <img src="../../frontend/IMG/Instagram.png">
+                    <span>Instagram</span>
+                </div>
+                <div class="contactoFace" id="contacto">
+                    <img src="../../frontend/IMG/facebook.png">
+                    <span>Facebook</span>
+                </div>
+                <div class="contactoWhats" id="contacto">
+                    <img src="../../frontend/IMG/WhatsApp.png">
+                    <span>WhatsApp</span>
+                </div>
+                <div class="contactoTel" id="contacto">
+                    <img src="../../frontend/IMG/llamada.png">
+                    <span>Telefono</span>
+                </div>
+            </section>
+        </aside>
 
-                <img src="../../frontend/IMG/llamada.png">
-                <span>Telefono</span>
+        <form method="POST" class="formulario">
+            <div class="contactoh2">
+                <img src="../../frontend/IMG/logo.png">
+                <h2>Contactanos</h2>
             </div>
-        </section>
-    </aside>
-
-    <form method="POST" class="contenedor">
-        <div class="contactoh2">
-            <img src="../../frontend/IMG/logo.png">
-            <h2>Contacta con nosotros</h2>
-        </div>
-        <div>
-            <span>Titulo</span><br>
-            <input type="text" alt="Titulo">
-        </div>
-        <div>
-            <span>Nombre Usuario</span><br>
-            <input type="text" alt="Nombre Usuario">
-        </div>
-        <div>
-            <span>Correo Electrónico</span><br>
-            <input type="email" alt="Correo Usuario">
-        </div>
-        <div>
-            <span>Mensaje</span><br>
-            <input type="text" alt="Descripción Mensaje">
-        </div>
-        <div>
-            <button class="btnEnviar">Enviar</button>
-        </div>
-    </form>
+            <div class="contenedorTitulo">
+                <span>Titulo</span><br>
+                <input class="titulo" type="text" alt="Titulo">
+            </div>
+            <div class="contenedorNombre">
+                <span>Nombre Usuario</span><br>
+                <input class="nombre" type="text" alt="Nombre Usuario">
+            </div>
+            <div class="contenedorCorreo">
+                <span>Correo Electrónico</span><br>
+                <input class="correo" type="email" alt="Correo Usuario">
+            </div>
+            <div class="contenedorMensaje">
+                <span>Mensaje</span><br>
+                <textarea class="mensaje" name="mensaje" rows="6" cols="85" placeholder="Escriba su mensaje"></textarea>
+            </div>
+            <div class="contenedorEnviar">
+                <button class="btnEnviar">Enviar</button>
+            </div>
+        </form>
+    </div>
 
 
 </body>
