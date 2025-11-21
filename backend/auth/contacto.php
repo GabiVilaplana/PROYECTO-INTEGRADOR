f1a907525238f9e6f2d117b1ce818518b351faa9
Sitoosito8 <a20000lex@gmail.com>
2025-11-21 17:17:19 +0100

    Subir cambios relacionados con la pagina de contacto.php

---
+++ backend/auth/contacto.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Contacto TaskLink</title>
+</head>
+
+<body>
+    <aside class="sidebar">
+        <section class="opcionesContacto">
+            <div class="contacto">
+                <img src="../../frontend/IMG/Instagram.png">
+                <span>Instagram</span>
+            </div>
+            <div class="contacto">
+                <img src="../../frontend/IMG/facebook.png">
+                <span>Facebook</span>
+            </div>
+            <div class="contacto">
+                <img src="../../frontend/IMG/WhatsApp.png">
+                <span>WhatsApp</span>
+            </div>
+            <div class="contacto">
+
+                <img src="../../frontend/IMG/llamada.png">
+                <span>Telefono</span>
+            </div>
+        </section>
+    </aside>
+
+    <form method="POST" class="contenedor">
+        <div class="contactoh2">
+            <img src="../../frontend/IMG/logo.png">
+            <h2>Contacta con nosotros</h2>
+        </div>
+        <div>
+            <span>Titulo</span><br>
+            <input type="text" alt="Titulo">
+        </div>
+        <div>
+            <span>Nombre Usuario</span><br>
+            <input type="text" alt="Nombre Usuario">
+        </div>
+        <div>
+            <span>Correo Electrónico</span><br>
+            <input type="email" alt="Correo Usuario">
+        </div>
+        <div>
+            <span>Mensaje</span><br>
+            <input type="text" alt="Descripción Mensaje">
+        </div>
+        <div>
+            <button class="btnEnviar">Enviar</button>
+        </div>
+    </form>
+
+
+</body>
+
+</html>
